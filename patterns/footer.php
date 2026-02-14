ca06cbc5c0cce41b70c643c0caf7ae1850165180
hdeplaen <henri.deplaen@gmail.com>
2026-02-14 19:45:40 +0100

    sleaker design

--- patterns/footer.php
+++ patterns/footer.php
@@ -8,104 +8,78 @@
 
 declare(strict_types=1) ?>
 
-<!-- wp:group {"templateLock":"all","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+<!-- wp:group {"templateLock":"all","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"tertiary","textColor":"contrast","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-contrast-color has-tertiary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:image {"width":"150px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="https://www2.olivaint.be/wp-content/themes/olivaint/assets/images/logo-black.webp" alt="Logo with text" style="width:150px;height:auto"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
 
-    <!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-    <div class="wp-block-group alignwide">
+<!-- wp:site-title {"align":"wide","style":{"typography":{"fontWeight":"700"}},"fontSize":"large","fontFamily":"playfair-display-sc"} /--></div>
+<!-- /wp:group -->
 
-        <!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
-        <div class="wp-block-group">
-        <!-- wp:image {"width":"150px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
-        <figure class="wp-block-image size-full is-resized">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo-white.png" alt="Logo with text" style="width:150px;height:auto"/>
-        </figure>
-        <!-- /wp:image -->
-        </div>
-        <!-- /wp:group -->
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"},"typography":{"lineHeight":"1"}},"fontSize":"x-small","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group has-x-small-font-size" style="line-height:1"><!-- wp:paragraph -->
+<p>Rue d'Egmont, 11<br>1000 Brussels</p>
+<!-- /wp:paragraph -->
 
-        <!-- wp:site-title {"level":1,"align":"wide","style":{"typography":{"fontFamily":"playfair-display-sc","fontWeight":"700"}},"fontSize":"large"} /-->
-    </div>
-    <!-- /wp:group -->
+<!-- wp:paragraph -->
+<p>+32 484 24 36 99</p>
+<!-- /wp:paragraph -->
 
-    <!-- wp:columns {"align":"wide"} -->
-    <div class="wp-block-columns alignwide">
-    	<!-- wp:column -->
-    	<div class="wp-block-column">
-    		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"},"typography":{"lineHeight":"1"}},"fontSize":"x-small","layout":{"type":"flex","orientation":"vertical"}} -->
-    		<div class="wp-block-group has-x-small-font-size" style="line-height:1">
-    			<!-- wp:paragraph -->
-    			<p><?php echo esc_html__("Rue d'Egmont, 11", "olivaint"); ?></p>
-    			<!-- /wp:paragraph -->
-    			<!-- wp:paragraph -->
-    			<p><?php echo esc_html__("1000 Brussels", "olivaint"); ?></p>
-    			<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>info@olivaint.be</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-    			<!-- wp:paragraph -->
-    			<p>+32 484 24 36 99</p>
-    			<!-- /wp:paragraph -->
-    			<!-- wp:paragraph -->
-    			<p>info@olivaint.be</p>
-    			<!-- /wp:paragraph -->
-    		</div>
-    		<!-- /wp:group -->
-    	</div>
-    	<!-- /wp:column -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:navigation-link {"label":"Cookie Policy","url":"#"} /-->
 
-    	<!-- wp:column -->
-    	<div class="wp-block-column">
-    		<!-- wp:columns -->
-    		<div class="wp-block-columns">
-    			<!-- wp:column {"width":"33.33%"} -->
-    			<div class="wp-block-column" style="flex-basis:33.33%">
-    				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
-    					<!-- wp:navigation-link {"label":"Cookie Policy","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"Privacy Policy","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"Policies","url":"#"} /-->
-    				<!-- /wp:navigation -->
-    			</div>
-    			<!-- /wp:column -->
+<!-- wp:navigation-link {"label":"Privacy Policy","url":"#"} /-->
 
-    			<!-- wp:column {"width":"33.33%"} -->
-    			<div class="wp-block-column" style="flex-basis:33.33%">
-    				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
-    					<!-- wp:navigation-link {"label":"Parent Portal","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"Pupil Portal","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"Stuff Portal","url":"#"} /-->
-    				<!-- /wp:navigation -->
-    			</div>
-    			<!-- /wp:column -->
+<!-- wp:navigation-link {"label":"Policies","url":"#"} /-->
+<!-- /wp:navigation --></div>
+<!-- /wp:column -->
 
-    			<!-- wp:column {"width":"33.33%"} -->
-    			<div class="wp-block-column" style="flex-basis:33.33%">
-    				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
-    					<!-- wp:navigation-link {"label":"Term Dates","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"School Calendar","url":"#"} /-->
-    					<!-- wp:navigation-link {"label":"Admissions","url":"#"} /-->
-    				<!-- /wp:navigation -->
-    			</div>
-    			<!-- /wp:column -->
-    		</div>
-    		<!-- /wp:columns -->
-    	</div>
-    	<!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:navigation-link {"label":"Parent Portal","url":"#"} /-->
 
-    <!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group alignwide">
-    	<!-- wp:social-links {"iconColor":"base","iconColorValue":"#064626","size":"has-small-icon-size","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|40"}}},"className":"is-style-logos-only"} -->
-    	<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only">
-    		<!-- wp:social-link {"url":"https://www.facebook.com/OlivaintBE","service":"facebook"} /-->
+<!-- wp:navigation-link {"label":"Pupil Portal","url":"#"} /-->
 
-    		<!-- wp:social-link {"url":"https://www.instagram.com/olivaint_be/","service":"instagram"} /-->
+<!-- wp:navigation-link {"label":"Stuff Portal","url":"#"} /-->
+<!-- /wp:navigation --></div>
+<!-- /wp:column -->
 
-    		<!-- wp:social-link {"url":"https://www.linkedin.com/school/conf%C3%A9rence-olivaint-genootschap","service":"linkedin"} /-->
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:navigation {"overlayMenu":"never","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:navigation-link {"label":"Term Dates","url":"#"} /-->
 
-    		<!-- wp:social-link {"url":"https://www.youtube.com/channel/UCA6152emRJtr1Pn6apwRleA","service":"youtube"} /-->
-    	</ul>
-    	<!-- /wp:social-links -->
-    </div>
-    <!-- /wp:group -->
-</div>
+<!-- wp:navigation-link {"label":"School Calendar","url":"#"} /-->
+
+<!-- wp:navigation-link {"label":"Admissions","url":"#"} /-->
+<!-- /wp:navigation --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#2a0606","size":"has-small-icon-size","className":"is-style-logos-only","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|40"}}}} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"https://www.facebook.com/OlivaintBE","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"https://www.instagram.com/olivaint_be/","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://www.linkedin.com/school/conf%C3%A9rence-olivaint-genootschap","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"https://www.youtube.com/channel/UCA6152emRJtr1Pn6apwRleA","service":"youtube"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header.php
+++ patterns/header.php
@@ -8,24 +8,12 @@
 
 declare(strict_types=1) ?>
 
-<!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","bottom":"0rem","left":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)","right":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"elements":{"link":{"color":{"text":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dbase)"}}}},"backgroundColor":"primary","textColor":"base","layout":{"type":"default"}} -->
-    <div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0rem;padding-right:var(--wp--preset--spacing--50);padding-bottom:0rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-        <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d30)","bottom":"0rem","left":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)","right":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)"}},"elements":{"link":{"color":{"text":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dbase)"},":hover":{"color":{"text":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dbase)"}}}}},"textColor":"base","layout":{"type":"default"}} -->
-            <div class="wp-block-group has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--50);padding-bottom:0rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:navigation {"ref":4,"overlayBackgroundColor":"primary","overlayTextColor":"base","style":{"typography":{"textTransform":"uppercase","letterSpacing":"0.03em"},"spacing":{"blockGap":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d40)"}},"fontSize":"x-small","layout":{"type":"flex","justifyContent":"center"}} /--></div>
-            <!-- /wp:group --></div>
-        <!-- /wp:group -->
+<!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"style":{"spacing":{"blockGap":"0"},"shadow":"6px 6px 9px rgba(0, 0, 0, 0.2)"},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group" style="box-shadow:6px 6px 9px rgba(0, 0, 0, 0.2)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0rem","bottom":"0rem","left":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)","right":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d50)"},"margin":{"top":"0","bottom":"0"},"blockGap":"0"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"default"}} -->
+<div class="wp-block-group has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:0rem;padding-right:var(--wp--preset--spacing--50);padding-bottom:0rem;padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"width":"0px","style":"none","radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-logo {"width":175,"shouldSyncIcon":false} /-->
 
-        <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-        <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-logo {"width":100,"shouldSyncIcon":false} /-->
-
-            <!-- wp:group {"style":{"spacing":{"blockGap":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dspacing\u002d\u002d30)"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
-            <div class="wp-block-group"><!-- wp:site-title {"level":0,"textAlign":"left","style":{"typography":{"textTransform":"none","fontWeight":"700","fontStyle":"normal"}},"fontSize":"x-large","fontFamily":"playfair-display-sc"} /-->
-
-                <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1","fontStyle":"italic","fontWeight":"600"}},"fontSize":"large","fontFamily":"playfair-display"} -->
-                <p class="has-playfair-display-font-family has-large-font-size" style="font-style:italic;font-weight:600;line-height:1">Royal Association</p>
-                <!-- /wp:paragraph --></div>
-            <!-- /wp:group --></div>
-        <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
+<!-- wp:navigation {"ref":4,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"600"}},"fontSize":"small","layout":{"type":"flex","justifyContent":"left"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->

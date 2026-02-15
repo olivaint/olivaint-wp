3451acc4d3a5ab5d66f70d5d5d276f4d2c2a65ac
hdeplaen <henri.deplaen@gmail.com>
2026-02-16 00:30:57 +0100

    minor

--- patterns/title.php
+++ patterns/title.php
@@ -9,27 +9,26 @@
 
 declare(strict_types=1) ?>
 
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":60,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":169,"align":"full","style":{"spacing":{"margin":{"top":"0"}}}} -->
-<div class="wp-block-cover alignfull" style="min-height:169px;margin-top:0">
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0"}}}} -->
+<div class="wp-block-group alignfull" style="margin-top:0">
 
-    <!-- wp:cover {"url":"","dimRatio":60,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":169,"minHeightUnit":"px","layout":{"type":"constrained"}} -->
-    <div class="wp-block-cover" style="min-height: 169px">
-        <span
-            aria-hidden="true"
-            class="wp-block-cover__background has-contrast-background-color has-background-dim-60 has-background-dim"
-        ></span>
-        <div class="wp-block-cover__inner-container">
-            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
-            <div class="wp-block-group">
-                <!-- wp:post-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} /-->
+  <!-- wp:cover {"url":"","dimRatio":60,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":169,"align":"full"} -->
+  <div class="wp-block-cover alignfull" style="min-height:169px">
+    <span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-60 has-background-dim"></span>
 
-                <!-- wp:paragraph {"placeholder":"Add subtitle…","fontSize":"medium"} -->
-                <p class="has-medium-font-size"></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
+    <div class="wp-block-cover__inner-container">
+      <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+      <div class="wp-block-group">
+        <!-- wp:post-title {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} /-->
+
+        <!-- wp:paragraph {"placeholder":"Add subtitle…","fontSize":"medium"} -->
+        <p class="has-medium-font-size"></p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
     </div>
-    <!-- /wp:cover -->
+  </div>
+  <!-- /wp:cover -->
+
 </div>
 <!-- /wp:group -->
